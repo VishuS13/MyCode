e8974041ef0a0c30cf7da461745dfe6c138fc1aa
VishuS13 <75628993+VishuS13@users.noreply.github.com>
2020-12-27 17:46:43 +0530

    food.c
    it is a menu of food items containing prices and names of food items.

---
+++ food.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("Enter your choice between 1-5:\n");
+scanf("%d",&n);
+switch(n)
+{
+case1:
+      printf("Food item-Pizza\nPrice-Rs 179\n");
+      break;
+case2:
+      printf("Food item-Burger\nPrice-Rs 129\n");
+      break;     
+case3:
+      printf("Food item-Pasta\nPrice-Rs 179\n");
+      break;
+case4:
+      printf("Food item-French Fries\nPrice-Rs 99\n");
+      break;      
+case5:
+      printf("Food item-Sandwich\nPrice-Rs 149\n");
+      break;      
+  }  
+      return 0;
+      }
